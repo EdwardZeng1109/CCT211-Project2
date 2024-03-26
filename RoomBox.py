d3806b302910cf65157e8bb231116747804e09f1
EdwardZeng1109 <116119165+EdwardZeng1109@users.noreply.github.com>
2024-03-25 23:45:28 -0400

    Organize code in multiple files

---
+++ RoomBox.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+
+class RoomBox:
+    def __init__(self, master):
+        self.frame = tk.Frame(master)
+        self.frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=5)
+
+        box_1 = tk.LabelFrame(self.frame, text="Room Availability", padx=5, pady=5)
+        box_1.pack(padx=10, pady=5, side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        box_2 = tk.LabelFrame(self.frame, text="Room Availability", padx=5, pady=5)
+        box_2.pack(padx=10, pady=5, side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        box_3 = tk.LabelFrame(self.frame, text="Room Availability", padx=5, pady=5)
+        box_3.pack(padx=10, pady=5, side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        box_4 = tk.LabelFrame(self.frame, text="Room Availability", padx=5, pady=5)
+        box_4.pack(padx=10, pady=5, side=tk.LEFT, fill=tk.BOTH, expand=True)
