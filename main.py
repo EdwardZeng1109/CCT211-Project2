f2a1b2c8832cb83c605801fa865c5865ffbe21e8
ShengyiF <162385539+ShengyiF@users.noreply.github.com>
2024-03-25 23:47:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import tkinter as tk
-from tkinter import ttk
-from ReservationBar import ReservationBar
-from RoomBox import RoomBox
-from InfoTable import InfoTable
+from reservation_bar import ReservationBar
+from info_table import InfoTable
+from room_box import RoomBox
 
-root = tk.Tk()
-root.title("Z Hotel Reservation System")
-root.geometry('1200x600')
+def main():
+    root = tk.Tk()
+    root.title("Z Hotel Reservation System")
+    root.geometry('1200x600')
 
-reservation_bar = ReservationBar(root)
-information_table = InfoTable(root)
-room_preview = RoomBox(root)
+    reservation_bar = ReservationBar(root)
+    information_table = InfoTable(root)
+    room_preview = RoomBox(root)
 
-root.mainloop()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
