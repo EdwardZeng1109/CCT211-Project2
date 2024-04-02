f9f38e5254b96c39c1b86f85ec43b98246539af3
ShengyiF <162385539+ShengyiF@users.noreply.github.com>
2024-04-02 15:15:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import tkinter as tk
 from reservation_bar import ReservationBar
 from info_table import InfoTable
 from room_box import RoomBox
-from login_system import App
+from login_system import Login
 
 def main():
     #log in system page
     root1 = tk.Tk()
     root1.title("Z Hotel Log in System")
     root1.geometry('1200x600')
-    login = App(root1, on_login_success = loged_in)
+    login = Login(root1, on_login_success = loged_in)
     root1.mainloop()
 
 def loged_in():
