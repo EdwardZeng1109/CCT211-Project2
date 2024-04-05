d9ab90fb70b6f75393a10c330e3fe752f48d50ec
ShengyiF <162385539+ShengyiF@users.noreply.github.com>
2024-04-05 01:13:36 -0400

    Update login_system.py

--- login_system.py
+++ login_system.py
@@ -20,12 +20,13 @@ class Login:
         self.password_entry = tk.Entry(self.root, show="*", width=20)
         self.password_entry.pack(padx=5, pady=5)
 
-        tk.Button(self.root, text="Login", command=self.check_login, font=("Courier New", 10, "bold"), bg="#274b6c",fg="white").pack(padx=10, pady=10)
+        tk.Button(self.root, text="Login", command=self.check_login, font=("Courier New", 10, "bold"), bg="#274b6c",
+                  fg="white").pack(padx=10, pady=10)
 
     def check_login(self):
         username = self.username_entry.get()
         password = self.password_entry.get()
-        if username == "1" and password == "1":
+        if username == "211" and password == "211":
             if self.on_login_success:
                 self.root.destroy() #Close the login window
                 self.on_login_success()
