f8bbfb7306fd04ce4db09fcdf4032645a7f1f493
ShengyiF <162385539+ShengyiF@users.noreply.github.com>
2024-04-04 21:02:02 -0400

    Update login_system.py

--- login_system.py
+++ login_system.py
@@ -7,20 +7,20 @@ class Login:
         self.root = root
 
         self.title_label = tk.Label(self.root, text="Z Hotel Reservation System",
-                                    font=("Courier New", 15, "bold"), fg="darkblue")
+                                    font=("Courier New", 15, "bold"), fg="#274b6c")
         self.title_label.pack(side=tk.TOP, padx=10, pady=15)
 
         self.on_login_success = on_login_success #check if user entered correct username and password
 
-        tk.Label(self.root, text="Username:", font=("Courier New", 10, "bold"), fg="darkblue").pack()
+        tk.Label(self.root, text="Username:", font=("Courier New", 10, "bold"), fg="#274b6c").pack()
         self.username_entry = tk.Entry(self.root, width=20)
         self.username_entry.pack(padx=5, pady=5)
 
-        tk.Label(self.root, text="Password:", font=("Courier New", 10, "bold"), fg="darkblue").pack()
+        tk.Label(self.root, text="Password:", font=("Courier New", 10, "bold"), fg="#274b6c").pack()
         self.password_entry = tk.Entry(self.root, show="*", width=20)
         self.password_entry.pack(padx=5, pady=5)
 
-        tk.Button(self.root, text="Login", command=self.check_login, font=("Courier New", 10, "bold"), bg="darkblue",fg="white").pack(padx=10, pady=10)
+        tk.Button(self.root, text="Login", command=self.check_login, font=("Courier New", 10, "bold"), bg="#274b6c",fg="white").pack(padx=10, pady=10)
 
     def check_login(self):
         username = self.username_entry.get()
